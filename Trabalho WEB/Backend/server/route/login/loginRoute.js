f2facb68a8fe97f6b2ac5ed7b5b6231ec353de83
Jefmaylon <jefmaylon@gmail.com>
2021-06-19 13:03:56 -0300

    Trabalho pronto

--- Trabalho WEB/Backend/server/route/login/loginRoute.js
+++ Trabalho WEB/Backend/server/route/login/loginRoute.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const loginService = require('../../service/login/loginService');
 
-router.get('/login/:usuario/:senha', async function (req, res) {
+router.get('/login/:usuario/:senha', async (req, res) => {
    const usuario = req.params.usuario;
    const senha = req.params.senha;
    const result = await loginService.getLogin(usuario, senha);
-   console.log(result);
+   res.json(result);
 });
 
 module.exports = router;
